5c4337799540e201dd4543fcbd7f3ec383619954
Phil Jansen <phil.k.jansen@gmail.com>
2020-03-03 20:10:42 -0800

    Add starvationCheck() to see if tasks stutter. Also some cleanup.

--- CPUmeter.ino
+++ CPUmeter.ino
@@ -10,8 +10,11 @@ class TimerPKJ
 {
   public:
     TimerPKJ(long period):  // in millisec
-      m_period(period), m_previous(0)
-    {};
+      m_period(period), m_overshoot(0)
+    {
+      m_previous = millis();
+      //m_previous = 0;
+    };
 
     bool ready(void)  // true when it's time to do something -- resets
     {
@@ -19,8 +22,8 @@ class TimerPKJ
       bool isReady = (m_period > 0) && (currentTime - m_previous >= m_period);
       if (isReady)
       {
-        //reset();
-        m_previous = currentTime;
+        reset();
+        //m_previous = currentTime;
       }
       return isReady;
     }
@@ -30,13 +33,21 @@ class TimerPKJ
       m_period = newPeriod;
     };
 
+    // reset timeout
     void reset(void)
     {
-      m_previous = millis();
+      auto currentTime = millis();
+      m_overshoot = currentTime - m_previous;
+      m_previous = currentTime;
     }
+
+    long overshoot(void) {
+      return m_overshoot;
+    };
   private:
     long m_period;
     unsigned long m_previous;
+    long m_overshoot;
 };
 
 //////////////////////////////////////////////////////////
@@ -54,26 +65,34 @@ class CPUmeter
       loopCount++;
       if (timer.ready())
       {
-        bestCase = max(bestCase, loopCount);
-        worstCase = min(worstCase, loopCount);
-        report(loopCount);
+        recentCase = loopCount;
         loopCount = 0;
+        bestCase = max(bestCase, recentCase);
+        worstCase = min(worstCase, recentCase);
+        //report(loopCount);
       }
     }
-    void report(long loopsToScale)
+
+    void longReport(void)
     {
       // more loops is lower CPU load absorbed by other tasks
-      int percentCPU = (int) (100. *(bestCase - loopsToScale) / (double)bestCase);
       auto seconds = (updateRate / 1000.);
 
-      Serial << "Best " << bestCase / seconds << " loops/sec; ";
-      Serial << "Worst " << worstCase / seconds << " loops/sec " << endl;
-      Serial << "current " << loopsToScale / seconds << " loops/sec " ;
+      Serial << "Best " << (int) (bestCase / seconds)
+             << " Worst " << (int) (worstCase / seconds)
+             << " current " << (int) (recentCase / seconds)
+             << " loops/sec " << endl;
+      report();
+    }
+    void report(void)
+    {
+      int percentCPU = (int) (100. *(bestCase - recentCase) / (double)bestCase);
       Serial << "CPU load at " << percentCPU << "%" << endl;
     };
 
   private:
     TimerPKJ timer;
+    long recentCase;
     long bestCase;
     long worstCase;
     long loopCount;
@@ -81,43 +100,99 @@ class CPUmeter
 };
 
 CPUmeter cpuMeter;
+TimerPKJ reporter(10000);
 
 //////////////////////////////////////////////////////////
-// waste a variable amount of time.
+// Waste a variable amount of time.
+// This is a well-behaved task
+// -- it sometimes has nothing to do so finishes quickly
+// -- it yields "often enough"
+//
 void wasteSomeTime(void)
 {
-  static int percentLoad = 0;
+  static int percentLoad = 0; // start with a low load
   static const int timeSlice = 10;
   static TimerPKJ importantWork(timeSlice);
   static TimerPKJ loadAdjuster(15000);
 
   if (importantWork.ready())
   {
-    if (random(1, 100) <= percentLoad)
+    if (random(0, 99) <= percentLoad)
     {
-      delay(timeSlice);
+      delay(timeSlice); // simulated important thing done!
     }
   }
-  
+
   if (loadAdjuster.ready())
   {
-    percentLoad -= 10; // change load 10%
+    percentLoad -= 10; // adjust load
     if (percentLoad < 0)
       percentLoad = 100; // wrap into 0-100%
-      Serial << "Load should be " << percentLoad << "%" << endl;
+    Serial << "Load set to " << percentLoad << "%" << endl;
   }
 }
 
+//////////////////////////////////////////////////////////
+// perform a "marginally long" maintenance task
+// Note that this may be long enough to (for example) make audio/video stutter
+//
+void maintenance(void)
+{
+  static TimerPKJ springCleaning(37000);
+
+  if (springCleaning.ready())
+  {
+    Serial << "archiving logs...";
+    delay(150); // simulated maintenance
+  }
+}
+
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+// are tasks starving a little?
+// helps determine if there's "enough" margin to meet
+// your requirements
+//
+// NOTE: This won't run/warn you if the task loop is completely blocked
+//
+void starvationCheck(void)
+{
+  static TimerPKJ deadline(20); // millisec to "sluggish" response
+
+  if (deadline.ready())
+  {
+    // detecting sluggish response..
+    Serial << "Refresh is LATE by " << deadline.overshoot() << "ms!" << endl;
+    // TODO:
+    // There may be a serious problem.
+    // Turn off motors, set brakes etc.
+  }
+  deadline.reset();
+}
 //////////////////////////////////////////////////////////
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  while (!Serial)
+    ; // for Leonardo USB...
+  Serial << "Ready." << endl;
 }
 
 //////////////////////////////////////////////////////////
 void loop() {
+
   // put your main code here, to run repeatedly:
 
-  wasteSomeTime();
-  cpuMeter.update();
+  wasteSomeTime();  // useful stuff...
+  maintenance();
+
+  cpuMeter.update();  // measure performance
+  starvationCheck();  // performance warning
+
+  if (reporter.ready())
+  {
+    cpuMeter.longReport();
+    //cpuMeter.report();
+  }
 }
